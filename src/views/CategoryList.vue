c9b5736f63c87fb9a6aa9c12770396da5745fdf2
diyashrestha1999 <diyashrestha1999@gmail.com>
2022-08-26 17:55:42 +0545

    friday work

--- src/views/CategoryList.vue
+++ src/views/CategoryList.vue
@@ -1,25 +1,68 @@
 <template>
-  <div class="category d-inline-flex">
-    <!-- <router-link to="/categoryproduct" style="text-decoration: none"> -->
-      <v-row>
-        <v-card
-          v-for="item in category"
-          :key="item.id"
-          class="card pa-5 ma-8"
-          width="500"
-          dark
+  <v-app>
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="orange"
+          class="white--text ma-4"
+          fab
+          small
+          right
+          top
+          v-bind="attrs"
+          v-on="on"
         >
-           <router-link
-              :to="{ name: 'CategoryProduct', params:{id:item.id} }"
-              style="text-decoration: none"
+          <v-icon small color="white">mdi-plus</v-icon>
+        </v-btn>
+      </template>
+      <v-card>
+        <form class="pa-12 white--text" @submit.prevent="addCategory">
+          <v-text-field
+            outlined
+            color="orange"
+            v-model="categoryName"
+            label="Category Name"
+          ></v-text-field>
+
+          <v-btn
+            class="mt-4 mr-4"
+            type="submit"
+            color="orange "
+            id="register"
+            value="Register"
+            dark
+          >
+            ADD
+          </v-btn>
+        </form>
+      </v-card>
+    </v-dialog >
+    <div class="category d-inline-flex">
+      <v-row>
+        <!-- <v-dialog v-model="dialog1" open-on-hover down offset-y > -->
+          <!-- <template v-slot:activator="{ on, attrs }"> -->
+            <v-card
+              v-for="item in category"
+              :key="item.id"
+              class="card pa-5 ma-8"
+              width="500"
+              dark
+
             >
-          <v-subheader><v-icon x-large>mdi-shape</v-icon></v-subheader>
-          <v-subheader>{{ item.name }}</v-subheader>
-          </router-link>
-        </v-card>
+              <router-link
+                :to="{ name: 'CategoryProduct', params: { id: item.id } }"
+                style="text-decoration: none"
+              >
+                <v-subheader><v-icon x-large>mdi-shape</v-icon></v-subheader>
+                <v-subheader>{{ item.name }}</v-subheader>
+              </router-link>
+            </v-card>
+          <!-- </template> -->
+          <v-icon small>mdi-delete</v-icon>
+        <!-- </v-dialog> -->
       </v-row>
-    <!-- </router-link> -->
-  </div>
+    </div>
+  </v-app>
 </template>
 <script>
 import axios from "axios";
@@ -27,7 +70,10 @@ import axios from "axios";
 export default {
   data() {
     return {
+      dialog: false,
+      dialog1:false,
       category: {},
+      categoryName: "",
     };
   },
   created() {
@@ -46,20 +92,34 @@ export default {
           console.log(response);
         });
     },
+    addCategory() {
+      axios
+        .post("http://127.0.0.1:8000/api/category/", {
+          name: this.categoryName,
+        })
+        .then((response) => {
+          console.log(response);
+          this.dialog = false;
+          // alert("sucessfull!!")
+        })
+        .catch((res) => console.log(res));
+    },
   },
 };
 </script>
 <style scoped>
 .card {
   background-color: rgba(0, 0, 0, 0.4);
-  border: black solid 1px;
+  border: white solid 1px;
 
-  transition: 0.5s;
+  transition: 0.1s;
 }
 .card:hover {
   box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
     rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
     rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
   transform: scale(1.02);
+
+  text-decoration-color: orange;
 }
 </style>
--- src/views/CategoryProduct.vue
+++ src/views/CategoryProduct.vue
@@ -1,23 +1,120 @@
 <template>
-    <h1>These are product in category</h1>
+  <v-app>
+    <v-container class="grey lighten-5">
+   
+      <router-link to="/addproduct" style="text-decoration: none">
+        <v-btn
+          color="orange"
+          class="white--text my-4"
+          fab
+          small
+          right
+          top
     
+        >
+          <v-icon small color="white">mdi-plus</v-icon>
+        </v-btn>
+      </router-link>
+
+      <!-- <h2 >{{products.category.name}}</h2> -->
+      <v-row>
+        <v-col v-for="product in products" :key="product.id" md="2">
+          <v-card
+            class="font rounded-lg"
+            elevation="5"
+            width="150"
+            height="200"
+          >
+            <v-hover v-slot="{ hover }">
+              <v-card class="mx-auto" max-width="600">
+                <v-img
+                  :aspect-ratio="16 / 9"
+                  src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
+                >
+                  <v-expand-transition>
+                    <div
+                      v-if="hover"
+                      class="
+                        d-flex
+                        transition-fast-in-fast-out
+                        orange
+                        v-card--reveal
+                        text-h6
+                        white--text
+                        text--darken-10
+                      "
+                      style="height: 100%"
+                    >
+                      ${{ product.price }}
+                    </div>
+                  </v-expand-transition>
+                </v-img>
+                <v-card-text class="pt-6" style="position: absolute">
+                  <ProductDialogVue :id="product.id" @delete="get_products"/>
+                  <h3
+                    class="
+                      font-weight-heavy
+                      orange--text
+                      mb-2
+                      text-center text-overline
+                    "
+                  >
+                    {{ product.name }}
+                  </h3>
+                </v-card-text>
+              </v-card>
+            </v-hover>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 <script>
-// import axios from 'axios'
+import ProductDialogVue from "@/components/ProductDialog.vue";
+import axios from "axios";
 
 export default {
-data(){
-    return{
+      components: {
+    ProductDialogVue,
+  },
+
+
+  data() {
+    return {
         id: this.$route.params.id,
-    }
-},
-methods:{
-    // getProductInCAtegory(){
-    //     axios({
-    //         method:"get",
-    //         url:`http://127.0.0.1:8000/api/product/{}`
-    //     })
-    // }
-}
-}
+        products: [],
+
+     
+    };
+  },
+
+  created() {
+    this.get_product_details();
+  },
+  methods: {
+    get_product_details() {
+      axios({
+        method: "get",
+        url: `http://localhost:8000/api/Productincategory/?category=${this.id}`,
+      })
+        .then((response) => {
+          this.products = response.data;
+        })
+        .catch((response) => {
+          console.log(response);
+        });
+    },
+  },
+};
 </script>
+<style scoped>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.5;
+  position: absolute;
+  width: 100%;
+}
+</style>
